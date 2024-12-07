6d2062be6d054772d7344717f924d2b2c8f32f77
antONget <a.l.ponomare@mail.ru>
2024-12-07 18:36:48 +0300

    init

--- handlers/group/service_messages.py
+++ handlers/group/service_messages.py
@@ -13,7 +13,7 @@ config: Config = load_config()
 
 @router.chat_member(F.new_chat_member)
 async def on_user_join(event: ChatMemberUpdated, bot: Bot):
-    logging.info(f'on_user_join {event.new_chat_member.status}')
+    logging.info(f'on_user_join {event.new_chat_member.status} {event}')
     if event.new_chat_member.status == 'member':
         await add_chat_user(tg_id=event.from_user.id,
                             first_name=event.from_user.first_name,
@@ -26,7 +26,7 @@ async def on_user_join(event: ChatMemberUpdated, bot: Bot):
                             last_rep_boost=datetime.datetime.now() - datetime.timedelta(hours=4),
                             last_help_boost=datetime.datetime.now() - datetime.timedelta(hours=4))
         await bot.send_message(chat_id=config.tg_bot.general_group,
-                               text=f'Привет  {event.from_user.full_name},'
+                               text=f'Привет  {event.new_chat_member.user.full_name},'
                                     f' почитай <a href="https://t.me/c/1327075982/69073/69075">правила группы</a>!',
                                message_thread_id=67828)
     elif event.new_chat_member.status == 'left':
